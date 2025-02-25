3166725c89277e0ad9c12e6ec75266386946f9e2
leejonggwon <gwon880328@gmail.com>
2025-02-26 04:07:00 +0900

    20250226_0406_commit

---
+++ 3. 중급 1편/workspace/java-mid1/src/lang/langMain.java
@@ -0,0 +1,9 @@
+package lang;
+//나의 언어로 받아들이기 때문에 생략해도 됨
+//import java.lang.System;
+
+public class langMain {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
