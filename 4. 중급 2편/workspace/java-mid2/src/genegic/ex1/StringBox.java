4208298581cf976e387536d0306ce9d4a5e8f2df
LeeJongGwon <gwon880328@gmail.com>
2025-05-05 09:43:16 +0900

    20250505_0943

---
+++ 4. 중급 2편/workspace/java-mid2/src/genegic/ex1/StringBox.java
@@ -0,0 +1,17 @@
+package genegic.ex1;
+
+public class StringBox {
+    //문자를 보관하는 박스
+
+    private String value;
+
+    public void set(String value) {
+        this.value = value;
+    }
+
+    public String get() {
+        return value;
+    }
+
+}
+
